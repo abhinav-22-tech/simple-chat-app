a414eaae3d09fa36cf962fd1ef044fbe6d7d60a1
abhinav-22-tech <jainabhinav0860@gmail.com>
2021-09-26 23:16:04 +0530

    deploy process  update

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const { Server } = require("socket.io");
 const io = new Server(server);
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static("../frontend"));
+// app.use(express.static("../frontend"));
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "../index.html");
