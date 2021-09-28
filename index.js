102f803b6301f2befeef82ab1f0b2bfff870c709
abhinav-22-tech <jainabhinav0860@gmail.com>
2021-09-28 19:21:07 +0530

    server setup separately

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import {io} from "socket.io-client";
 
-const socket = io("http://localhost:3000");
+const socket = io("https://simple-chats-server.herokuapp.com/");
 
 socket.on("init", handleInit);
 
